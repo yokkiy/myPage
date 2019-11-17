af210006970ad3bdfb7bddd1dd4e727d867d66d1
yokkiy <16fi088@ms.dendai.ac.jp>
2019-11-17 18:08:04 +0900

    デザイン改修中

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
 
   get '/' => "home#top"
-  get 'post/index' => "post#index"
+  get 'posts/index' => "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
