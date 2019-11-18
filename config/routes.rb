620ec09aa9699a5d0291db084c1c6cb0a27d7387
yokkiy <16fi088@ms.dendai.ac.jp>
2019-11-18 14:46:14 +0900

    統一部分をアプリケーションコントローラに統一

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-   resources :blogs
+ resources :blogs
    root 'home#top'
   get 'posts/index' => "posts#index"
 
   get '/' => "home#top"
-  get 'posts/index' => "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
