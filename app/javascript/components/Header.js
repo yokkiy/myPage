a2e4859559746708d0ead6f55cc380be00e86282
yokkiy <16fi088@ms.dendai.ac.jp>
2020-01-01 20:41:27 +0900

    リンクできるようにした(途中)

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,15 +1,28 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 class Header extends React.Component {
   render () {
     return (
+    <BrowserRouter>
       <div class="header-contents-wrapper">
-        <ui>
-        <li>リンクA</li>
-        <li>リンクB</li>
-        </ui>
+       <ul>
+        <li><Link to='/'>ホーム</Link></li>
+        <li><Link to='/posts/introduction'>自己紹介</Link></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/'>Home</Link></li>
+       </ul>
+
+
+
+       <Route exact path='/' />
+       <Route path='/posts/introduction' />
       </div>
+
+
+
+    </BrowserRouter>
     );
   }
 }
