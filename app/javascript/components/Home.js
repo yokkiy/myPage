5aa0d9e4637cdcc2c24584825ea83d3fd3c68249
yokkiy <16fi088@ms.dendai.ac.jp>
2020-01-02 21:08:32 +0900

    UI調整

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -5,8 +5,8 @@ class Home extends React.Component {
   render () {
     return (
       <div class="twitter">
-
           <a class="twitter-timeline" data-lang="ja" data-width="300" data-height="500" data-theme="dark" data-link-color="#E81C4F" href="https://twitter.com/mochopuyon?ref_src=twsrc%5Etfw">Tweets by mochopuyon</a>
+          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
       </div>
     );
